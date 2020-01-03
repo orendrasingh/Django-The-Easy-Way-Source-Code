a913fba1a70b1283953fbd52cb166f0108180432
Samuli Natri <samuli.natri@gmail.com>
2020-01-03 12:07:15 +0200

    update

---
+++ 18-Deployment-PythonAnywhere/blog/context_processors.py
@@ -0,0 +1,8 @@
+from blog.models import Post
+
+
+def latest_posts(request):
+
+    posts = Post.objects.filter().order_by('-date')[:5]
+
+    return {'latest_posts': posts}
---
+++ 18-Deployment-PythonAnywhere/blog/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from .models import Post
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title',
+                  'body',
+                  'slug',
+                  'tags',
+                  'image']
+
+class PostDeleteForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = []
