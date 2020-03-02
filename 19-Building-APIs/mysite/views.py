1866a3e911c8f4e4e06782403e87d8f0d14b0989
Samuli Natri <samuli.natri@gmail.com>
2020-03-02 15:58:57 +0200

    update

---
+++ 19-Building-APIs/mysite/views.py
@@ -0,0 +1,67 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status, permissions
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Blog
+from .serializers import BlogSerializer
+
+
+class CustomPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.has_perm('mysite.add_blog'):
+            return True
+        return False
+
+
+@api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+@permission_classes([CustomPermission])
+def blog_api_view(request):
+    if request.method == "GET":
+
+        paginator = PageNumberPagination()
+        paginator.page_size = 1
+        blog_objects = Blog.objects.all()
+        result = paginator.paginate_queryset(blog_objects,
+                                             request)
+
+        # serializer = BlogSerializer(Blog.objects.all(),
+        #                            many=True)
+        serializer = BlogSerializer(result, many=True)
+
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def blog_api_detail_view(request, pk=None):
+    try:
+        blog = Blog.objects.get(pk=pk)
+    except blog.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = BlogSerializer(blog,
+                                    data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(blog.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 <img align="left" width="100" src="data/images/cover.png">
 
-<h4>[Updated 2020.01.02] Works with Django 2 and 3!</h4>
+<h4>[Updated 2020.03.01] Works with Django 2 and 3!</h4>
 
 [LeanPub (Free updates)](https://leanpub.com/django-the-easy-way)
 
@@ -10,18 +10,6 @@
 
 [Amazon (Paperback)](https://amzn.to/38CwCse)
 
-## About the book
-
-<img align="left" width="300" src="data/images/Screenshot1.png">
-
-<h4>Key Features</h4>
-
-o Beginner-friendly <b>function-based</b> approach.
-
-o Covers all <b>essential concepts</b>.
-
-<br><br>
-
 ## About the author
 
-Samuli Natri is a programmer who specialises in Django, Python and JavaScript development. He attended Helsinki University of Technology (Computer Science) and Helsinki University (Social Sciences).
+Samuli Natri is a software developer. He studied computer science at Helsinki University Of Technology.
